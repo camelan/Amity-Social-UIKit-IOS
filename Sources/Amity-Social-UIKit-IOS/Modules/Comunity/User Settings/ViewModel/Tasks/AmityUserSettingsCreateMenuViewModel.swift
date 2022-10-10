ac56d8acfaec2a4989b8bac2124ca8c0c81c810d
Mohammed Badr <mohammed.badr@camelan.com>
2022-10-10 13:39:19 +0200

    Disabled User to update his profile from Amity

--- Sources/Amity-Social-UIKit-IOS/Modules/Comunity/User Settings/ViewModel/Tasks/AmityUserSettingsCreateMenuViewModel.swift
+++ Sources/Amity-Social-UIKit-IOS/Modules/Comunity/User Settings/ViewModel/Tasks/AmityUserSettingsCreateMenuViewModel.swift
@@ -22,10 +22,10 @@ final class AmityUserSettingsCreateMenuViewModel: AmityUserSettingsCreateMenuVie
             
             settingsItems.append(.header(content: manageItemHeader))
             
-            let editProfile = AmityUserSettingsItem.editProfile
-            let editProfileItem = AmitySettingsItem.NavigationContent(identifier: editProfile.identifier, icon: editProfile.icon, title: editProfile.title, description: nil)
-            
-            settingsItems.append(.navigationContent(content: editProfileItem))
+//            let editProfile = AmityUserSettingsItem.editProfile
+//            let editProfileItem = AmitySettingsItem.NavigationContent(identifier: editProfile.identifier, icon: editProfile.icon, title: editProfile.title, description: nil)
+//            
+//            settingsItems.append(.navigationContent(content: editProfileItem))
             
             settingsItems.append(.separator)
             
